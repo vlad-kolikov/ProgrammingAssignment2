8252ea09c26654039b0c47331d462eb9d6fea782
vlad-kolikov <vlad-kolikov@i.ua>
2014-05-24 23:52:19 +0300

    Functions makeCacheMatrix and cacheSolve were complited.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions below are used to create a special object that stores
+## a numeric matrix and caches its inverse, and to calculate its inverse
+## or to get it from cache if it has been already calculated.
 
-## Write a short comment describing this function
+## The function makeCacheMatrix creates a special "matrix", which is really
+## a list containing functions to:
+## 1) set the value of the matrix
+## 2) get the value of the matrix
+## 3) set the value of the inverse of the matrix
+## 4) get the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        invm <- NULL
+        set <- function(y) {
+                x <<- y
+                invm <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) invm <<- inverse
+        getinverse <- function() invm
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The function cacheSolve calculates the inverse of the special "matrix"
+## created with the above function. However, it first checks via the
+## getinverse function to see if the iverse has already been calculated and 
+## matrix didn't change since last calculation: this is done by check of
+## dimensions equality and matrix product equality to identity matrix.
+## If so, it gets the inverse from the cache and skips the computation.
+## Otherwise, it calculates the inverse of the data and sets the value
+## of the inverse in the cache via the setinverse function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getinverse()
+        if(!is.null(inverse)) {
+                dimid <- identical(dim(inverse), dim(x$get()))
+                mid <- identical(inverse %*% x$get(), diag(dim(inverse)[1]))
+                if(dimid & mid) {
+                        message("getting cached data")
+                        return(inverse)
+                }
+                
+        }
+        data <- x$get()
+        inverse <- solve(data, ...)
+        x$setinverse(inverse)
+        inverse
 }
